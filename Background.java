de82725c0dc6c24cec2a0ec66f10a4e9d8a8afec
ThunderRabbit <zuzazibubabi@gmail.com>
2025-02-16 11:14:07 +0800

    doing the mud, this is hella hard lmao

--- Background.java
+++ Background.java
@@ -5,7 +5,6 @@ import javax.swing.*;
 public class Background extends JPanel {
 
     public Background() {
-        // Set preferred size to 800x600
         setPreferredSize(new Dimension(800, 600));
     }
 
@@ -14,16 +13,67 @@ public class Background extends JPanel {
         private Shape[] shapes;
         private Color[] colors;
 
-        public Mud(double x, double y) {
+        public Mud(double x, double y, String type) {
+
+            if(type.equals("normal")) { 
             // Define the colors for the mud rectangles
-            colors = new Color[] { new Color(0x896a30), new Color(0x6f4e27), new Color(0x4e342e) };
+                colors = new Color[] { new Color(0x473818), new Color(0x896a30), new Color(0x74552a), new Color(0x6a492a), new Color(0x6c4b28), new Color(0x7b5a2d), new Color(0x7b5a2d),
+                     new Color(0x5b4123), new Color(0x5b4123), 
+                     new Color(0x614227), new Color(0x614227),
+                     new Color(0x482e1c), new Color(0x513520), new Color(0x4a301c),
+                    };
 
             // Define the positions and sizes of the mud rectangles
             shapes = new Shape[] {
+                new Rectangle2D.Double(x, y-1, 9, 1),
                 new Rectangle2D.Double(x, y, 9, 2),
-                new Rectangle2D.Double(x + 10, y, 9, 2),
-                new Rectangle2D.Double(x + 20, y, 9, 2)
+                new Rectangle2D.Double(x, y+1, 9, 1),
+                new Rectangle2D.Double(x, y+2, 9, 2),
+                new Rectangle2D.Double(x+2, y+4, 5, 3),
+                new Rectangle2D.Double(x-1, y, 1, 2),
+                new Rectangle2D.Double(x+9, y, 1, 2),
+                new Rectangle2D.Double(x-1, y+1, 1, 3),
+                new Rectangle2D.Double(x+9, y+1, 1, 3),
+                new Rectangle2D.Double(x+2, y+2, 1, 3),
+                new Rectangle2D.Double(x+6, y+2, 1, 3),
+                
+                //shadow
+
+                new Rectangle2D.Double(x+5, y+7, 4, 3),
+                new Rectangle2D.Double(x+7, y+4, 4, 3),
+                new Rectangle2D.Double(x+10, y+2, 2, 4),
+
+            
             };
+
+
+
+
+
+            }
+            
+            else if(type.equals("withShadow")) {
+                // Define the colors for the mud rectangles
+                colors = new Color[] { new Color(0x473818), new Color(0x896a30), new Color(0x74552a), new Color(0x6a492a), new Color(0x6c4b28), new Color(0x7b5a2d), new Color(0x7b5a2d),
+                    new Color(0x5b4123), new Color(0x5b4123), 
+                    new Color(0x614227), new Color(0x614227) 
+                   };
+
+           // Define the positions and sizes of the mud rectangles
+           shapes = new Shape[] {
+               new Rectangle2D.Double(x, y-1, 9, 1),
+               new Rectangle2D.Double(x, y, 9, 2),
+               new Rectangle2D.Double(x, y+1, 9, 1),
+               new Rectangle2D.Double(x, y+2, 9, 2),
+               new Rectangle2D.Double(x+2, y+4, 5, 3),
+               new Rectangle2D.Double(x-1, y, 1, 2),
+               new Rectangle2D.Double(x+9, y, 1, 2),
+               new Rectangle2D.Double(x-1, y+1, 1, 3),
+               new Rectangle2D.Double(x+9, y+1, 1, 3),
+               new Rectangle2D.Double(x+2, y+2, 1, 3),
+               new Rectangle2D.Double(x+6, y+2, 1, 3),
+           };
+        }
         }
 
         public Shape[] getShapes() {
@@ -68,10 +118,17 @@ public class Background extends JPanel {
             g2d.fill(clouds[i]);
         }
 
-        Color[] floorColors = {brownMain};
+        Color[] floorColors = {brownMain, new Color(000000), new Color(0x1b1f24), new Color(0x382615), new Color(0x6d4c29), new Color(0x462f1d), new Color(0x332019)};
 
         Shape[] floor = {
             new Rectangle2D.Double(0, 470, 800, 130),
+            new Rectangle2D.Double(0, 468, 800, 2),
+            new Rectangle2D.Double(0, 467, 800, 1),
+            new Rectangle2D.Double(0, 470, 800, 1),
+            new Rectangle2D.Double(0, 471, 800, 2),
+            new Rectangle2D.Double(0, 473, 800, 1),
+            new Rectangle2D.Double(0, 474, 800, 14),
+
         };
 
         // Paint each floor layer with the corresponding color
@@ -82,8 +139,27 @@ public class Background extends JPanel {
 
         // Define mud shapes with colors
         Mud[] mud = {
-            new Mud(62, 471),
-            new Mud(500, 471)
+            new Mud(7, 471, "normal"),
+            new Mud(18, 471, "withShadow"),
+            new Mud(32, 471, "normal"),
+            new Mud(48, 471, "withShadow"),
+            new Mud(62, 471, "normal"),
+            new Mud(75, 471, "normal"),
+            new Mud(92, 471, "withShadow"),
+            new Mud(105, 471, "withShadow"),
+            new Mud(119, 471, "normal"),
+            new Mud(119, 471, "withShadow"),
+            new Mud(135, 471, "withShadow"),
+            new Mud(149, 471, "normal"),
+
+
+
+
+
+
+
+
+
         };
 
         // Paint each mud shape
